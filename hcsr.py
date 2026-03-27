c832be305b237af03af8996763ab66bf96d6a0d2
Paco <pacoman1608@gmail.com>
2026-03-26 21:57:14 -0600

    prueba sensores HCSR-04

---
+++ hcsr.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+TRIG = 23
+ECHO = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.output(TRIG, False)
+print("Waiting for sensor to settle...")
+time.sleep(2)
+
+try:
+    while True:
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
+
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17165
+        distance = round(distance, 1)
+
+        print(f"Distance: {distance} cm")
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    print("Measurement stopped by User")
+finally:
+    GPIO.cleanup()Copied!   
